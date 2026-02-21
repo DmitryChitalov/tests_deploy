bbe3fc36919dff7a0a7ee40c4318a1fd73e8ea12
Selectel Git User <git-user@selectel.ru>
2026-02-21 16:35:11 +0500

    sixth

--- conftest.py
+++ conftest.py
@@ -4,10 +4,16 @@ from page.login_page import LoginPage
 from test_data.data_provider import DataProvider
 from configuration.config_provider import ConfigProvider
 
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument("--headless")  # Запуск без графического интерфейса
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
 
 @fixture(autouse=True)
 def driver(config):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.timeouts.implicit_wait = config.get_int("ui", "timeout_seconds")
     yield driver
     if driver is not None:
